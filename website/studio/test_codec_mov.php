8e6bef950accca9073ed7befb74ad9c9e5fc6bf9
George Williams <george.williams@gmail.com>
2012-02-23 17:29:55 -0500

    test codes

---
+++ website/studio/test_codec_mov.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+         <video id="theVideo" width="480" height="282" poster="animatics_cinematics/1new.png" controls="controls" preload="auto" style="visibility:hidden">
+            <source type="video/mp4" src="animatics_cinematics/sd/1new.mov" />
+	    <h1>MOV file will not load</h1>    
+        </video>
+</body>
+</html>
