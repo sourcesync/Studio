c1ed33e113af44c30bfca4066ac5a7abc3c6b455
George Williams <george.williams@gmail.com>
2012-02-27 15:52:28 -0500

    stuff

---
+++ website/animation/shorts/test_codec_webm.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+         <video id="theVideo" width="480" height="282" poster="0.png" controls="controls" preload="auto" style="visibility:hidden">
+            <source type="video/webm" src="sd/0.webmvp8.webm" />
+	    WEBM file will not load
+        </video>
+</body>
+</html>
