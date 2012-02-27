e12d2b7b6e5b65c162169c1c90740a99c1561d94
George Williams <george.williams@gmail.com>
2012-02-27 00:48:53 -0500

    test

---
+++ website/animation/shorts/test_codec_mov.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+         <video id="theVideo" width="480" height="282" poster="0.png" controls="controls" preload="auto" style="visibility:hidden">
+            <source type="video/mp4" src="sd/0.mov" />
+	    <h1>MOV file will not load</h1>    
+        </video>
+</body>
+</html>
