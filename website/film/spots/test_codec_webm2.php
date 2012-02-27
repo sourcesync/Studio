38929521dde1eaca9f72f6a1fa4b25cd5441216c
George Williams <george.williams@gmail.com>
2012-02-27 16:06:26 -0500

    Tests

---
+++ website/film/spots/test_codec_webm2.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+         <video id="theVideo" width="480" height="282" poster="0.png" controls="controls" preload="auto">
+            <source type="video/webm" src="sd/GlobalFund_framed_wm.webm" />
+	    WEBM file will not load    
+        </video>
+</body>
+</html>
