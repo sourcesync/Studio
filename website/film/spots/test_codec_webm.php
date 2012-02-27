9abe9456fe726832dd19da454d4b8674d4b938a0
George Williams <george.williams@gmail.com>
2012-02-27 15:04:41 -0500

    test

---
+++ website/film/spots/test_codec_webm.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+         <video id="theVideo" width="480" height="282" poster="animatics_cinematics/1new.png" controls="controls" preload="auto">
+            <source type="video/webm" src="test.webm" />
+	    <h1>WEBM file will not load</h1>    
+        </video>
+</body>
+</html>
