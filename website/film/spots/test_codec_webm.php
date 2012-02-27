31a2042940430dcebb0e7ab3274503ce0cd04728
George Williams <george.williams@gmail.com>
2012-02-27 18:08:47 -0500

    stuff

--- website/film/spots/test_codec_webm.php
+++ website/film/spots/test_codec_webm.php
@@ -1,8 +1,8 @@
 <html>
 <body>
-         <video id="theVideo" width="480" height="282" poster="0.png" controls="controls" preload="auto">
+         <video id="theVideo" width="480" height="282" poster="0.png" controls="controls" preload="auto" >
             <source type="video/webm" src="0.webmvp8.webm" />
-	    WEBM file will not load    
+	    WEBM file will not load
         </video>
 </body>
 </html>
--- website/film/spots/test_codec_webm3.php
+++ website/film/spots/test_codec_webm3.php
@@ -1,7 +1,7 @@
 <html>
 <body>
          <video id="theVideo" width="480" height="282" poster="0.png" controls="controls" preload="auto">
-            <source type="video/webm" src="/film/film/sd/100.webmvp8.webm" />
+            <source type="video/webm" src="/animation/shorts/sd/0.webmvp8.webm" />
 	    WEBM file will not load    
         </video>
 </body>
